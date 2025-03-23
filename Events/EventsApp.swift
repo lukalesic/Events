c4f018f7ceeaada252b61dd1f15d8264a2ffb6e6
lukalesic <lukalesic20@gmail.com>
2025-03-23 14:40:32 +0100

    Initial Commit

---
+++ Events/EventsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventsApp.swift
+//  Events
+//
+//  Created by Luka Lešić on 23.03.25.
+//
+
+import SwiftUI
+
+@main
+struct EventsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
